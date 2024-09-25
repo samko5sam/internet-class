f629971121b39fd171ce5834dd37353a5b58f61c
sk5s <samko5sam@tutanota.com>
2024-09-26 00:02:23 +0800

    ✨ Add voo and vxus, fix layout

--- docs/assets/js/stock.js
+++ docs/assets/js/stock.js
@@ -8,16 +8,46 @@ const proxy_prefix = "https://corsproxy.io/?";
 
 document.addEventListener("DOMContentLoaded", async () => {
   if (false) return;
-  const response = await fetch(`${proxy_prefix}https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_0050.tw&json=1&delay=0&lang=zh_tw`);
-  const data = await response.json();
-  console.log(data);
-  console.log(data.msgArray[0].o);
-  const tse_0050_price_df = (data.msgArray[0].pz - data.msgArray[0].y).toFixed(2);
-  if (tse_0050_price_df > 0){
-    document.getElementById("tse_0050_price").style.color = red;
-  } else {
-    document.getElementById("tse_0050_price").style.color = green;
+  let upAndDownIndicator = ""
+
+  const upAndDownHandler = (df, el, el2) => {
+    if (df > 0){
+      el.style.color = red;
+      el2.style.color = red;
+      upAndDownIndicator = "🔺"
+    } else {
+      el.style.color = green;
+      el2.style.color = red;
+      upAndDownIndicator = "🔻"
+    }
   }
+
+  // 0050
+  let response = await fetch(`${proxy_prefix}https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_0050.tw&json=1&delay=0&lang=zh_tw`);
+  let data = await response.json();
+  let price_df = (data.msgArray[0].pz - data.msgArray[0].y).toFixed(2);
+  upAndDownHandler(price_df, document.getElementById("tse_0050_price"), document.getElementById("tse_0050_price_df"));
+  let percentage = (price_df / data.msgArray[0].y * 100).toFixed(2);
   document.getElementById("tse_0050_price").innerText = parseFloat(data.msgArray[0].pz).toFixed(2);
-  document.getElementById("tse_0050_price_df").innerText = tse_0050_price_df;
+  document.getElementById("tse_0050_price_df").innerText = `${upAndDownIndicator}${price_df} (${percentage}%)`;
+
+  // VOO
+  response = await fetch(`${proxy_prefix}https://query1.finance.yahoo.com/v8/finance/chart/VOO?interval=1m&lang=en-US&region=US`);
+  data = await response.json();
+  console.log(data);
+  price_df = (data.chart.result[0].meta.regularMarketPrice - data.chart.result[0].meta.chartPreviousClose).toFixed(2);
+  upAndDownHandler(price_df, document.getElementById("voo_price"), document.getElementById("voo_price_df"));
+  percentage = (price_df / data.chart.result[0].meta.chartPreviousClose * 100).toFixed(2);
+  document.getElementById("voo_price").innerText = parseFloat(data.chart.result[0].meta.regularMarketPrice).toFixed(2);
+  document.getElementById("voo_price_df").innerText = `${upAndDownIndicator}${price_df} (${percentage}%)`;
+
+  // VXUS
+  response = await fetch(`${proxy_prefix}https://query1.finance.yahoo.com/v8/finance/chart/VXUS?interval=1m&lang=en-US&region=US`);
+  data = await response.json();
+  console.log(data);
+  price_df = (data.chart.result[0].meta.regularMarketPrice - data.chart.result[0].meta.chartPreviousClose).toFixed(2);
+  upAndDownHandler(price_df, document.getElementById("vxus_price"), document.getElementById("vxus_price_df"));
+  percentage = (price_df / data.chart.result[0].meta.chartPreviousClose * 100).toFixed(2);
+  document.getElementById("vxus_price").innerText = parseFloat(data.chart.result[0].meta.regularMarketPrice).toFixed(2);
+  document.getElementById("vxus_price_df").innerText = `${upAndDownIndicator}${price_df} (${percentage}%)`;
 })
